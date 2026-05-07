381e0846364586a7fbd3f15d7307dd05581d2488
chenaoh <3138125060javier@gmail.com>
2026-05-06 19:20:15 -0500

    con el siguiente comando creamos las carpetas categorias y produtos, (nest g res productos --no-spec) con ese codigo en la terminal creamos la carpetas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { join } from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { CategoriasModule } from './categorias/categorias.module';
+import { ProductosModule } from './productos/productos.module';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    CategoriasModule,
+    ProductosModule,
   ],
 })
 export class AppModule {}
